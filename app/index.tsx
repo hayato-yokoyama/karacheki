c9946c7c2f3f0f808a282dfacac3de3d5270470d
dovicie <hyokoyama8810@gmail.com>
2024-11-16 11:44:25 +0900

    💄

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Card, H2, H3, Paragraph, YStack } from "tamagui";
 export default function Index() {
   return (
     <YStack padding="$8" gap="$8">
-      <H2>体重</H2>
+      <H2>Weekly</H2>
       <YStack gap="$4">
         <Card elevate size="$4" p="$4">
           <H3>今週</H3>
@@ -14,7 +14,7 @@ export default function Index() {
           <Paragraph>68.3 kg</Paragraph>
         </Card>
         <Card elevate size="$4" p="$4">
-          <H3>移動平均 (差分)</H3>
+          <H3>差分</H3>
           <Paragraph>+1.3 kg</Paragraph>
         </Card>
       </YStack>
