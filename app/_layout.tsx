d9e3526e7f3b3a27e1892c37b22112542f03b315
dovicie <hyokoyama8810@gmail.com>
2024-11-16 11:39:59 +0900

    ➕ init tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,41 @@
+import tamaguiConfig from "@/tamagui.config";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { TamaguiProvider } from "@tamagui/core";
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+
+  const [loaded] = useFonts({
+    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      // can hide splash screen here
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="details" />
-    </Stack>
+    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="details" />
+        </Stack>
+      </ThemeProvider>
+    </TamaguiProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,23 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Card, H2, H3, Paragraph, YStack } from "tamagui";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Link href="/details/1">View first user details</Link>
-      <Link href="/details/2">View second user details</Link>
-    </View>
+    <YStack padding="$8" gap="$8">
+      <H2>体重</H2>
+      <YStack gap="$4">
+        <Card elevate size="$4" p="$4">
+          <H3>今週</H3>
+          <Paragraph>70.0 kg</Paragraph>
+        </Card>
+        <Card elevate size="$4" p="$4">
+          <H3>先週</H3>
+          <Paragraph>68.3 kg</Paragraph>
+        </Card>
+        <Card elevate size="$4" p="$4">
+          <H3>移動平均 (差分)</H3>
+          <Paragraph>+1.3 kg</Paragraph>
+        </Card>
+      </YStack>
+    </YStack>
   );
 }
