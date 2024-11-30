340bf8fc5e4b47e8b1b0b9d31890f87ee84c0847
dovicie <hyokoyama8810@gmail.com>
2024-11-30 11:17:10 +0900

    💄 ナビバー微修正

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,20 +2,14 @@ import { Tabs } from "expo-router";
 
 import Ionicons from "@expo/vector-icons/Ionicons";
 
-import { ChartLine, Home } from "@tamagui/lucide-icons";
+import { ChartLine, Home, Settings } from "@tamagui/lucide-icons";
 
 export default function TabLayout() {
 	return (
 		<Tabs
 			screenOptions={{
-				tabBarActiveTintColor: "#ffd33d",
-				headerStyle: {
-					backgroundColor: "#25292e",
-				},
-				headerShadowVisible: false,
-				headerTintColor: "#fff",
-				tabBarStyle: {
-					backgroundColor: "#25292e",
+				tabBarLabelStyle: {
+					fontSize: 8,
 				},
 			}}
 		>
@@ -23,14 +17,14 @@ export default function TabLayout() {
 				name="index"
 				options={{
 					title: "ホーム",
-					tabBarIcon: ({ color }) => <Home color={color} />,
+					tabBarIcon: ({ color }) => <Home color={color} size="$1" />,
 				}}
 			/>
 			<Tabs.Screen
 				name="graph/index"
 				options={{
 					title: "グラフ",
-					tabBarIcon: ({ color }) => <ChartLine color={color} />,
+					tabBarIcon: ({ color }) => <ChartLine color={color} size="$1" />,
 				}}
 			/>
 		</Tabs>
